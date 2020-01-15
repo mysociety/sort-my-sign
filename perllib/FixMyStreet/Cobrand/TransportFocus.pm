462984e086933d2deff50613beb644b7a6907343
M Somerville <matthew-github@dracos.co.uk>
2020-01-15 17:58:11 +0000

    Send acknowledgement email.

--- perllib/FixMyStreet/Cobrand/TransportFocus.pm
+++ perllib/FixMyStreet/Cobrand/TransportFocus.pm
@@ -23,6 +23,8 @@ sub send_questionnaires { 0 }
 
 sub enable_category_groups { 1 }
 
+sub report_sent_confirmation_email { 'id' }
+
 sub body {
     FixMyStreet::DB->resultset('Body')->search({ name => 'Highways England' })->first;
 }
