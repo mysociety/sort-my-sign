d1f18712d221748f900ea7edbab2128f8f685cdb
M Somerville <matthew-github@dracos.co.uk>
2020-01-16 10:37:33 +0000

    Heatmap.

--- perllib/FixMyStreet/Cobrand/TransportFocus.pm
+++ perllib/FixMyStreet/Cobrand/TransportFocus.pm
@@ -20,6 +20,8 @@ sub enable_category_groups { 1 }
 
 sub report_sent_confirmation_email { 'id' }
 
+sub reports_ordering { 'created-desc' }
+
 sub report_form_extras { (
     { name => 'how_long', required => 1 },
     { name => 'consent', required => 1 },
@@ -29,6 +31,8 @@ sub report_form_extras { (
 sub body {
     FixMyStreet::DB->resultset('Body')->search({ name => 'Highways England' })->first;
 }
+
+sub dashboard_permission { $_[0]->body->id }
 
 sub area_check {
     my ( $self, $params, $context ) = @_;
