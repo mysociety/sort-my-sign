1ad8bed04a0d4ffff495215fc449c266d5acbd18
M Somerville <matthew-github@dracos.co.uk>
2020-01-27 11:51:13 +0000

    Extra columns in CSV export.

--- perllib/FixMyStreet/Cobrand/TransportFocus.pm
+++ perllib/FixMyStreet/Cobrand/TransportFocus.pm
@@ -123,4 +123,40 @@ sub _lookup_db {
     }
 }
 
+sub dashboard_export_problems_add_columns {
+    my $self = shift;
+    my $c = $self->{c};
+
+    splice @{$c->stash->{csv}->{headers}}, 5, 0, 'Subcategory';
+    splice @{$c->stash->{csv}->{columns}}, 5, 0, 'subcategory';
+
+    $c->stash->{csv}->{headers} = [
+        grep { $_ !~ /Acknowledged|Fixed|Closed|Status|Site Used|Reported As/ }
+        map {
+            if ($_ eq 'Ward') { 'Region' }
+            elsif ($_ eq 'Title') { 'Where' }
+            else { $_ }
+        } @{ $c->stash->{csv}->{headers} },
+        "Road",
+        "How long",
+    ];
+
+    $c->stash->{csv}->{columns} = [
+        grep { $_ !~ /acknowledged|fixed|closed|state|site_used|reported_as/ }
+        @{ $c->stash->{csv}->{columns} },
+        "road_name",
+        "how_long",
+    ];
+
+    $c->stash->{csv}->{extra_data} = sub {
+        my $report = shift;
+        my $fields = {
+            road_name => $report->get_extra_metadata('road_name'),
+            how_long => $report->get_extra_metadata('how_long'),
+            subcategory => $report->get_extra_metadata('subcategory') || '',
+        };
+        return $fields;
+    };
+}
+
 1;
